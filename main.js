5da12a555490271612592a2c184919cdc67a2928
Dawid <dawidwojcik476@gmail.com>
2021-03-14 19:49:59 +0100

    Already done with responsive on all devices

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const menuBtn = document.querySelector('.menu-btn');
 const mainMenu = document.querySelector('.menu');
+const menuHideBtn = document.querySelectorAll('.menu li')
 
 let menuOpen = false;
 
@@ -15,4 +16,15 @@ const menu = () => {
     }
 }
 
+
+const scroll = new SmoothScroll('a[href*="#"]');
+
+
 menuBtn.addEventListener('click', menu);
+menuHideBtn.forEach(function (button) {
+
+    button.addEventListener('click', function () {
+        menuBtn.classList.remove('open');
+        mainMenu.classList.remove('open');
+    })
+})
